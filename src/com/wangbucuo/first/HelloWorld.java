1caec0fd67e26c5b0aacd7e391a63aad6700b353
wangbucuo01 <2537854178@qq.com>
2024-01-21 15:45:31 +0800

    hello world

---
+++ src/com/wangbucuo/first/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.wangbucuo.first;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a = 100;
+        System.out.println(a + " hello world");
+    }
+}
